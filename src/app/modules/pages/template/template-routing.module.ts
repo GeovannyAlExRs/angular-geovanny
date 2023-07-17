164eff77e9c6bbf8e1b1702a7dc29e585c20ddfb
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-17 15:05:15 -0500

    add routes path Profile in the template routing

--- src/app/modules/pages/template/template-routing.module.ts
+++ src/app/modules/pages/template/template-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', loadChildren: () => import('@modules/pages/template/profile/profile.module').then(m => m.ProfileModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
