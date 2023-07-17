49bb02349686d9c6b3ee334e4010e8b323708399
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-17 10:19:25 -0500

    menu in the component header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  mainMenu: { defaultOptions: Array<any> } = { defaultOptions: [] }
+
+  @Output() menuToggle = new EventEmitter<void>()
+  @Output() signOut = new EventEmitter<void>()
+  @Input() isAuthorized!: boolean | null
+  //@Input() users!: UsersResponse | null
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    this.mainMenu.defaultOptions = [
+      { name: 'Home', icon: 'uil uil-estate', router: ['/'], band: !this.isAuthorized },
+      { name: 'Profile', icon: 'uil uil-brightness-plus', router: ['/'], band: false },
+      { name: 'Register', icon: 'uil uil-user-plus', router: ['/'], band: this.isAuthorized },
+      { name: 'Login', icon: 'uil uil-signin', router: ['/'], band: this.isAuthorized },
+      { name: 'Sign out', icon: 'uil uil-signout', router: ['/'], band: !this.isAuthorized }
+    ]
+  }
+
+  menuToggleDispatch() : void {
+    this.menuToggle.emit()
+  }
+
+  logoutUser(menu: string): void {
+    if(menu === 'Sign out') {
+      this.signOut.emit()
+    }
+  }
 }
