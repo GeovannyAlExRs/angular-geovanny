30b60cca4dbde361edb3b33b994e25afbda64971
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-17 14:03:10 -0500

    imports modules angular material in the module sidebar

--- src/app/shared/components/sidebar/sidebar.module.ts
+++ src/app/shared/components/sidebar/sidebar.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
 
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     SidebarComponent
