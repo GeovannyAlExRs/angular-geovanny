a49b9c16aa956012588562c1ebd707e492381a9d
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-17 14:54:40 -0500

    add routers path in the auth

--- src/app/modules/pages/auth/auth-routing.module.ts
+++ src/app/modules/pages/auth/auth-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('@modules/pages/auth/template/login/login.module').then(m => m.LoginModule)},
+  { path: 'register', loadChildren: () => import('@modules/pages/auth/template/register/register.module').then(m => m.RegisterModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
