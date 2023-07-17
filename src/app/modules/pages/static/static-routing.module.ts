ad677f013c6a6e447e16165043fc30e5d982f93f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-17 14:34:07 -0500

    add path in the routing Static

--- src/app/modules/pages/static/static-routing.module.ts
+++ src/app/modules/pages/static/static-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('@modules/pages/static/template/home/home.module').then(m => m.HomeModule)},
+  { path: '404', loadChildren: () => import('@modules/pages/static/template/not-found/not-found.module').then(m => m.NotFoundModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
