af38977a607f810596e851c8aaf7cf95b63083d8
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-17 14:07:22 -0500

    implement sidebar component

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
+  menuList: { accessLink: Array<any> } = { accessLink: [] }
+
+  @Output() menuToggle = new EventEmitter<void>()
+  @Output() signOut = new EventEmitter<void>()
+  @Input() isAuthorized!: boolean | null
+  //@Input() users!: UsersResponse | null
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.menuList.accessLink = [
+      { name: 'Home', icon: 'uil uil-estate', router: ['/'], band: !this.isAuthorized },
+      { name: 'Profile', icon: 'uil uil-brightness-plus', router: ['/'], band: false },
+      { name: 'Register', icon: 'uil uil-user-plus', router: ['/'], band: this.isAuthorized },
+      { name: 'Login', icon: 'uil uil-signin', router: ['/'], band: this.isAuthorized },
+      { name: 'Sign out', icon: 'uil uil-signout', router: ['/'], band: !this.isAuthorized }
+    ]
+  }
+
+  closeMenu() :void {
+    this.menuToggle.emit()
+  }
+
+  logoutUser(menu: string): void {
+    if(menu === 'Sign out') {
+      this.signOut.emit()
+      //console.log('Menu: SOY', menu)
+    }
+  }
 }
